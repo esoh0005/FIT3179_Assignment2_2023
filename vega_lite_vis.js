97b5a54aadda99ea014d097a4affb93fe37a2eb5
En Ming Soh <145523234+esoh0005@users.noreply.github.com>
2023-10-11 13:18:50 +0800

    Removed action

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -4,23 +4,23 @@ var vg_3 = "js/multiple_views.vg.json";
 var vg_4 = "js/circle_happiness.vg.json";
 var vg_5 = "js/bubble_chart_happiness.vg.json";
 
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1,{"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#boxplot", vg_2).then(function(result) {
+vegaEmbed("#boxplot", vg_2,{"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#multiple", vg_3).then(function(result) {
+vegaEmbed("#multiple", vg_3,{"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#circle", vg_4).then(function(result) {
+vegaEmbed("#circle", vg_4,{"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#bubble", vg_5).then(function(result) {
+vegaEmbed("#bubble", vg_5,{"actions": false}).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
